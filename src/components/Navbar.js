e0965ca77d9046b879b645554e14ab1b8fa05fef
Sakthikumar08 <sakthikumarcricket@gmail.com>
2025-03-11 22:36:52 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <h2 className="logo">Smart building management system</h2>
-        <nav>
+       {/*  <nav>
         <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
         <NavLink to="/complaint-box" className="nav-link" activeClassName="active">Complaint Box</NavLink>
         <NavLink to="/complaint-report" className="nav-link" activeClassName="active">Complaint Report</NavLink>
         <NavLink to="/analytics" className="nav-link" activeClassName="active">Analytics</NavLink>
-      </nav>  
+      </nav>  */} 
     </div>
   );
 };
